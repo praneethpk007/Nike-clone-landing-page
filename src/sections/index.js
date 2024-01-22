9f677366128510dd05ce663f677f960f045fbd50
praneethpk007 <210020014@iitdh.ac.in>
2024-01-23 02:37:15 +0530

    First Commit: Added NavBar and Hero

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Nav from '../components/Nav'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+
+export{
+    Hero,
+    CustomerReviews,
+    Footer,
+    Nav,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality
+}
