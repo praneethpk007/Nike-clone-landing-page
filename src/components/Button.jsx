b4fda6ed5f51703c212b6f4ba6e86410f8aaded2
praneethpk007 <210020014@iitdh.ac.in>
2024-01-24 02:16:45 +0530

    Added ShoeCard, PopularProducts, SuperQuality, Services, SpecialOffer sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,18 @@
-const Button = ({ label, iconUrl}) => {
+const Button = ({ label, iconUrl, backgroundColor, borderColor, textColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+    ${backgroundColor 
+      ? `${backgroundColor} ${textColor} ${borderColor}`  
+      : "bg-coral-red text-white border-coral-red"
+    } rounded-full w-full`}
+  >
         {label}
-        <img
+        
+        {iconUrl && <img
             src={iconUrl} 
             alt="Arrow Right Icon"
             className="ml-2 rounded-full w-5 h-5"
-        />
+        />}
     </button>
   )
 }
